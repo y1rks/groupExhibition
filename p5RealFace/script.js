ef2b1cddbcb6cd9405b68e0269682df4582eed25
y1rks <yu.martian@gmail.com>
2020-09-27 14:34:44 +0900

    Change count number

--- p5RealFace/script.js
+++ p5RealFace/script.js
@@ -7,12 +7,12 @@ function mainProcess() {
   const img3 = document.getElementById("image3");
   const img4 = document.getElementById("image4");
 
-  if (count === 5) {
+  if (count === 4) {
     img1.src = "./images/1/1.BMP";
     img2.src = "./images/1/2.BMP";
     img3.src = "./images/1/3.BMP";
     img4.src = "./images/1/4.BMP";
-  } else if (count === 10) {
+  } else if (count === 8) {
     img1.src = "./images/2/1.BMP";
     img2.src = "./images/2/2.BMP";
     img3.src = "./images/2/3.BMP";
