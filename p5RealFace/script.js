a3fab753c9e10a85ee1d8bb23a3ebaa012b71105
y1rks <yu.martian@gmail.com>
2020-11-04 01:29:19 +0900

    Add black

--- p5RealFace/script.js
+++ p5RealFace/script.js
@@ -1,5 +1,6 @@
 let imgState = true;
 let count = 0;
+let blackCount = 0;
 
 function mainProcess() {
   const img1 = document.getElementById("image1");
@@ -7,33 +8,50 @@ function mainProcess() {
   const img3 = document.getElementById("image3");
   const img4 = document.getElementById("image4");
 
-  if (count === 4) {
-    img1.src = "./images/1/1.BMP";
-    img2.src = "./images/1/2.BMP";
-    img3.src = "./images/1/3.BMP";
-    img4.src = "./images/1/4.BMP";
-  } else if (count === 8) {
-    img1.src = "./images/2/1.BMP";
-    img2.src = "./images/2/2.BMP";
-    img3.src = "./images/2/3.BMP";
-    img4.src = "./images/2/4.BMP";
-    count = 0;
+  if (blackCount > 60) {
+    img1.src = "./images/black/black.bmp";
+    img2.src = "./images/black/black.bmp";
+    img3.src = "./images/black/black.bmp";
+    img4.src = "./images/black/black.bmp";
+
+    if (count === 8) {
+      count = 0;
+    }
+
   } else {
-    if (imgState) {
-      img1.src = "./images/2/1.BMP";
+    if (count === 4) {
+      img1.src = "./images/1/1.BMP";
       img2.src = "./images/1/2.BMP";
       img3.src = "./images/1/3.BMP";
-      img4.src = "./images/2/4.BMP";
-    } else {
-      img1.src = "./images/1/1.BMP";
+      img4.src = "./images/1/4.BMP";
+    } else if (count === 8) {
+      img1.src = "./images/2/1.BMP";
       img2.src = "./images/2/2.BMP";
       img3.src = "./images/2/3.BMP";
-      img4.src = "./images/1/4.BMP";
+      img4.src = "./images/2/4.BMP";
+      count = 0;
+    } else {
+      if (imgState) {
+        img1.src = "./images/2/1.BMP";
+        img2.src = "./images/1/2.BMP";
+        img3.src = "./images/1/3.BMP";
+        img4.src = "./images/2/4.BMP";
+      } else {
+        img1.src = "./images/1/1.BMP";
+        img2.src = "./images/2/2.BMP";
+        img3.src = "./images/2/3.BMP";
+        img4.src = "./images/1/4.BMP";
+      }
     }
+  }
+
+  if (blackCount > 90) {
+    blackCount = 0;
   }
 
   imgState = !imgState;
   count++;
+  blackCount++;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
